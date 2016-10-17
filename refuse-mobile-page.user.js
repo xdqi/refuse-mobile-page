cc4e213d946ce78a7d5919b4a1c64016199d467b
Alexander Qi <xdqi@users.noreply.github.com>
2016-10-17 21:52:22 +0800

    Bump 0.2

--- refuse-mobile-page.user.js
+++ refuse-mobile-page.user.js
@@ -4,7 +4,7 @@
 // @description        Redirect mobile-only URL to its corresponding PC version automatically
 // @description:zh-CN  访问移动设备专用 URL 时跳转到对应的 PC 版页面（目前已支持微博、淘宝、天猫、京东）
 // @namespace          https://github.com/xdqi/refuse-mobile-page
-// @version            0.1
+// @version            0.2
 // @author             xdqi
 // @grant              none
 // @run-at             document-start
